290f7a50ed6d4b4cb96162d658d4901a51f4ccfe
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-04-06 17:07:55 +0530

    app file code added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 
 
 export default function App() {
+  const arr = new Array(64).fill(0);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div style={{width:"60%", height:"auto", backgroundColor:"blue", color:"white", display:"grid",gridTemplateColumns:"repeat(8, 1fr)", gridTemplateRows:"repeat(8,1fr)", justifyContent:"center", alignItems:"center"}}>
+      {arr.map((el,i)=> <div style={{border:"1px solid red", margin:"20px"}}>{i+1}</div> )}
+    </div>
   )
 }
